e067c2dd20dbb735e737d3b07410481d52bd88d9
Suraj Shrestha <shresthasuraj62@gmail.com>
2020-06-16 10:29:20 -0700

    Added home page styles

--- src/components/Home.js
+++ src/components/Home.js
@@ -7,8 +7,9 @@ const Home =()=>{
         <div id="home-div" className="home-div">
             <img src={photo} alt="Suraj Shrestha" className="my-image"/>
             <div className="info-text">
-                <h1 className="name">Suraj Shrestha</h1>
-                <h1 className="position">Full Stack Developer</h1>
+                <p className="name"><span className="hi">Hi,</span><span className="i-am"> I am </span>Suraj Shrestha</p>
+                <p className="position">Full Stack Developer</p>
+                <p className="goal"><i>To obtain a professional position in the field of Computer Science utilizing my relevant experience, technical expertise and problem-solving skills.</i></p>
 
             </div>
         </div>
