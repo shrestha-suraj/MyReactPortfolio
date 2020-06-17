b77b5130bee71b0ecb409b44e49e0d8fbfbee617
Suraj Shrestha <shresthasuraj62@gmail.com>
2020-06-17 09:46:56 -0700

    Done with home screen

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,17 +1,16 @@
 import React from 'react'
 import './styles/home.css'
-import photo from './media/me.png'
+import {DownCircleOutlined} from '@ant-design/icons';
 
 const Home =()=>{
     return (
-        <div id="home-div" className="home-div">
-            <img src={photo} alt="Suraj Shrestha" className="my-image"/>
+        <div id="home-div">
             <div className="info-text">
                 <p className="name"><span className="hi">Hi,</span><span className="i-am"> I am </span>Suraj Shrestha</p>
                 <p className="position">Full Stack Developer</p>
-                <p className="goal"><i><span>"</span>To obtain a professional position in the field of Computer Science utilizing my relevant experience, technical expertise and problem-solving skills.<span>"</span></i></p>
-
+                <p className="goal"><i><span>"</span>Let's get to know me, shall we?<span>"</span></i></p>
             </div>
+            <a href="#profile-div" className="go-button"><DownCircleOutlined/></a>
         </div>
     )
 }
